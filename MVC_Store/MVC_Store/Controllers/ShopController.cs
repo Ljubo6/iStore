84bab566fc28d6f48fa536e0a9a702f3ac3cfb14
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2020-03-29 15:31:44 +0300

    iStore 19

--- MVC_Store/MVC_Store/Controllers/ShopController.cs
+++ MVC_Store/MVC_Store/Controllers/ShopController.cs
@@ -2,6 +2,7 @@
 using MVC_Store.Models.ViewModels.Shop;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -28,6 +29,93 @@ namespace MVC_Store.Controllers
             //Връщаме частичното View
 
             return PartialView("_CategoryMenuPartial", categoryVMList);
+        }
+
+        // GET: Shop/Category/name
+        public ActionResult Category(string name)
+        {
+            //Обявяваме списък тип List
+            List<ProductVM> productVMList;
+            using (Db db = new Db())
+            {
+                //Получаваме ID категории
+
+                CategoryDTO categoryDTO = db.Categories.Where(x => x.Slug == name).FirstOrDefault();
+
+                int catId = categoryDTO.Id;
+
+                //Инициализираме списък с данните
+
+                productVMList = db.Products.ToArray().Where(x => x.CategoryId == catId).Select(x => new ProductVM(x)).ToList();
+
+                //Получаваме име на категориите
+
+                var productCat = db.Products.Where(x => x.CategoryId == catId).FirstOrDefault();
+
+                //Правим проверка за null
+
+                if (productCat == null)
+                {
+                    var catName = db.Categories.Where(x => x.Slug == name).Select(x => x.Name).FirstOrDefault();
+
+                    ViewBag.CategoryName = catName;
+                }
+                else
+                {
+                    ViewBag.CategoryName = productCat.CategoryName;
+                }
+
+            }
+
+            //Връщаме View с модела
+
+            return View(productVMList);
+        }
+
+        // GET: Shop/product-details/name
+        [ActionName("product-details")]
+        public ActionResult ProductDetails(string name)
+        {
+            //Обявяваме модел DTO  и VM
+
+            ProductDTO dto;
+            ProductVM model;
+
+            //Инициализираме Id на продукта
+
+            int id = 0;
+
+            using (Db db = new Db())
+            {
+
+                //Проверяваме продукта на достъпност
+
+                if (!db.Products.Any(x => x.Slug.Equals(name)))
+                {
+                    return RedirectToAction("Index","Shop");
+                }
+
+                //Инициализираме модела DTO даннии
+
+                dto = db.Products.Where(x => x.Slug == name).FirstOrDefault();
+
+                //Получаваме Id
+
+                id = dto.Id;
+
+                //Инициализираме модела VM данни
+
+                model = new ProductVM(dto);
+
+            }
+            //Получаваме изобравение от галерията
+
+            model.GalleryImages = Directory.EnumerateFiles(Server.MapPath("~/Images/Uploads/Products/" + id + "/Gallery/Thumbs"))
+                .Select(fn => Path.GetFileName(fn));
+
+            //Връщаме модела във View
+
+            return View("ProductDetails",model);
         }
     }
 }
