3c424e16b3d2c1d7c78e8a61f193e37df4ef65b1
Lyubomir Lyubomirov <Ljubo6@abv.bg>
2020-03-30 17:49:07 +0300

    upload all

---
+++ MVC_Store/MVC_Store/Areas/Admin/Controllers/PagesController.cs
@@ -0,0 +1,325 @@
+﻿using MVC_Store.Models.Data;
+using MVC_Store.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Store.Areas.Admin.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Admin/Pages
+        public ActionResult Index()
+        {
+            //Обявяваме списък за View - PageVM
+            List<PageVM> pageList;
+
+            //Инициализираме списък (DB)
+
+            using (Db db = new Db())
+            {
+                pageList = db.Pages.ToArray().OrderBy(x => x.Sorting).Select(x => new PageVM(x)).ToList();
+            }
+
+            //Връщаме списък с View
+
+            return View(pageList);
+        }
+
+        // GET: Admin/Pages/AddPage
+        [HttpGet]
+        public ActionResult AddPage()
+        {
+            return View();
+        }
+        // POST: Admin/Pages/AddPage
+        [HttpPost]
+        public ActionResult AddPage(PageVM model)
+        {
+            //Проверка на моделите за валидност
+
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            using (Db db = new Db())
+            {
+
+
+                //Обявяваме променлива за кратко описание Slug
+                string slug;
+
+                //Инициализираме класа PageDTO
+
+                PagesDTO dto = new PagesDTO();
+
+                //Присвояваме заглавие на модела
+                dto.Title = model.Title.ToUpper();
+
+                //Проверка има ли кратко описание и ако не го просвояваме
+
+                if (string.IsNullOrWhiteSpace(model.Slug))
+                {
+                    slug = model.Title.Replace(" ", "-").ToLower();
+                }
+                else
+                {
+                    slug = model.Slug.Replace(" ","-").ToLower();
+                }
+
+                //Убеждаваме се ,че заглавието и краткото описание са уникални
+
+                if (db.Pages.Any(x => x.Title == model.Title))
+                {
+                    ModelState.AddModelError("","That title already exist.");
+                    return View(model);
+                }
+                else if (db.Pages.Any(x => x.Slug == model.Slug))
+                {
+                    ModelState.AddModelError("", "That slug already exist.");
+                    return View(model);
+                }
+
+                //Присвояваме останалите значения на модделите
+
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+                dto.Sorting = 100;
+
+                //Съхранчваме модел в базата данни
+
+                db.Pages.Add(dto);
+                db.SaveChanges();
+            }
+
+            //Съобщение чрез TempData
+
+            TempData["SM"] = "You have added new page!";
+
+            //Преадресиране на потребителя към метод Idex
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: Admin/Pages/EditPage/id
+        [HttpGet]
+        public ActionResult EditPage(int id)
+        {
+            //Обявяваме модел PageVM
+            PageVM model;
+
+            using (Db db = new Db())
+            {
+                //Получаваме данни за страницата
+
+                PagesDTO dto = db.Pages.Find(id);
+
+                //Проверяваме дали е достъпна въобще тази страница
+
+                if (dto == null)
+                {
+                    return Content("The Page does not exist.");
+                }
+
+                //Инициализираме модела за данните
+
+                model = new PageVM(dto);
+            }
+            
+            //Връщаме View в модела
+            return View(model);
+        }
+
+        // POST: Admin/Pages/EditPage
+        [HttpPost]
+        public ActionResult EditPage(PageVM model)
+        {
+            //Проверяваме модела за валидност
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            using (Db db = new Db())
+            {
+                //получаваме ID  на стъраницата 
+                int id = model.Id;
+
+                //Обявяваме променлива за нашето кратко заглавие , т.е това е локална променлива
+                string slug = "home";
+
+                //Получаваме страницата по ID
+                PagesDTO dto = db.Pages.Find(id);
+
+                //Присвоявам названието от получените модели в DTO
+                dto.Title = model.Title;
+
+                //Проверяваме нашият Slug дали го има и го присвояваме ако е необходимо
+                if (model.Slug != "home")
+                {
+                    if (string.IsNullOrWhiteSpace(model.Slug))
+                    {
+                        slug = model.Title.Replace(" ","-").ToLower();
+                    }
+                    else
+                    {
+                        slug = model.Slug.Replace(" ","-").ToLower();
+                    }
+                }
+
+                //Проверяваме Slug и Title  за уникалност
+                if (db.Pages.Where(x => x.Id != id).Any(x => x.Title == model.Title))
+                {
+                    ModelState.AddModelError("","The title already exist.");
+                    return View(model);
+                }
+                else if (db.Pages.Where(x => x.Id != id).Any(x => x.Slug == slug))
+                {
+                    ModelState.AddModelError("", "The slug already exist.");
+                    return View(model);
+                }
+
+                //Записваме останалите значения в клас DTO
+
+                dto.Slug = slug;
+                dto.Body = model.Body;
+                dto.HasSidebar = model.HasSidebar;
+
+                //Записваме в базата
+
+                db.SaveChanges();
+            }
+
+            //Отмятаме съобщение чрез TempData
+
+            TempData["SM"] = "You have edited the page.";
+
+            //Преадресация на потребител
+
+            return RedirectToAction("EditPage");
+        }
+
+
+        // GET: Admin/Pages/PageDetails/id
+        public ActionResult PageDetails(int id)
+        {
+            //Обявяваме модела PageVM
+            PageVM model;
+
+            using (Db db = new Db())
+            {
+                //Получаваме страницата
+                PagesDTO dto = db.Pages.Find(id);
+
+                //Убеждаваме се ,че страницата е достъпна
+                if (dto == null)
+                {
+                    return Content("The page does not exist.");
+                }
+
+                //Присвояваме на нашият модел всички  полета от  базата
+                model = new PageVM(dto);
+
+            }
+
+            //Връщаме модела във  View - то
+            return View(model);
+        }
+
+
+        // GET: Admin/Pages/DeletePage/id
+        public ActionResult DeletePage(int id)
+        {
+            using (Db db = new Db())
+            {
+                //Почаваме страницата
+                PagesDTO dto = db.Pages.Find(id);
+
+                //Изтриваме страницата
+                db.Pages.Remove(dto);
+
+                //Записваме изменетияте в базата
+
+                db.SaveChanges();
+            }
+
+            //Добавяме съобщение за успешно изтриване
+            TempData["SM"] = "You have deleted a page";
+            //Преадресираме потребителя
+            return RedirectToAction("Index");
+        }
+
+
+
+        //Създаваме метод за сортиране
+        // POST: Admin/Pages/ReorderPages
+
+        [HttpPost]
+        public void ReorderPages(int[] id)
+        {
+            using (Db db = new Db())
+            {
+                //Реализираме начален брояч
+                int count = 1;
+                //Инициализираме модела за данни
+                PagesDTO dto;
+                //Правим сортировка за всяка страница
+                foreach (var pageId in id)
+                {
+                    dto = db.Pages.Find(pageId);
+                    dto.Sorting = count;
+
+                    db.SaveChanges();
+                    count++;
+                }
+
+            }
+        }
+
+        // GET: Admin/Pages/EditSidebar
+        [HttpGet]
+        public ActionResult EditSidebar()
+        {
+            //Обябяваме модела
+            SidebarVM model;
+            using (Db db = new Db())
+            {
+                //Получаваме данните от DTO
+                SidebarDTO dto = db.Sidebars.Find(1);//Лаянян код
+                //Запълваме модела
+                model = new SidebarVM(dto);
+            }
+            //Връщаме View  заедно с модела
+            return View(model);
+        }
+
+        // POST: Admin/Pages/EditSidebar
+        [HttpPost]
+        public ActionResult EditSidebar(SidebarVM model)
+        {
+            using (Db db = new Db())
+            {
+                //Взимаме данни от базата чрез DTO
+                SidebarDTO dto = db.Sidebars.Find(1); //Лаянян код
+
+                //Прицвияваме данните в тялото(в свойство body)
+                dto.Body = model.Body;
+
+                //Запис
+
+                db.SaveChanges();
+            }
+
+            TempData["SM"] = "You have edited sidebar";
+
+            //Присвояваме съобщение в TempData
+
+            //Преадресиране
+            return RedirectToAction("EditSidebar");
+        }
+
+    }
+}
---
+++ MVC_Store/MVC_Store/Controllers/PagesController.cs
@@ -0,0 +1,96 @@
+﻿using MVC_Store.Models.Data;
+using MVC_Store.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Store.Controllers
+{
+    public class PagesController : Controller
+    {
+        //(Урок 17)
+        // GET: Index/page{}
+        public ActionResult Index(string page = "")
+        {
+            //Създаваме умалено име(Slug)
+            if (page == "")
+            {
+                page = "home";
+            }
+            //Обявяваме модел и клас DTO
+            PageVM model;
+            PagesDTO dto;
+
+            //Проверяваме дали е достъпна текущата страница
+            using (Db db = new Db())
+            {
+                if (!db.Pages.Any(x => x.Slug.Equals(page)))
+                {
+                    return RedirectToAction("Index",new { page = ""});
+                }
+            }
+            //Получаваме DTO на странивцата
+            using (Db db = new Db())
+            {
+                dto = db.Pages.Where(x => x.Slug == page).FirstOrDefault();
+            }
+            //Установяваме заглавието на страницата (Title)
+
+            ViewBag.PageTitle = dto.Title;
+
+            //Проверяваме страничният панел
+
+            if (dto.HasSidebar == true)
+            {
+                ViewBag.Sidebar = "Yes";
+            }
+            else
+            {
+                ViewBag.Sidebar = "No";
+            }
+            //Запълваме модела с данни
+
+            model = new PageVM(dto);
+
+            //Връщаме View с модела
+
+            return View(model); 
+        }
+
+        public ActionResult PagesMenuPartial()
+        {
+            //Инициализираме List PageVM
+            List<PageVM> pageVMList;
+            //Получаваме всички страници освен HOME
+            using (Db db = new Db())
+            {
+                pageVMList = db.Pages
+                    .ToArray()
+                    .OrderBy(x => x.Sorting)
+                    .Where(x => x.Slug != "home")
+                    .Select(x => new PageVM(x))
+                    .ToList();
+            }
+            //Връщаме частично View с лист от данни
+            return PartialView("_PagesMenuPartial", pageVMList);
+        }
+
+        public ActionResult SidebarPartial()
+        {
+            //Обявяваме модел
+            SidebarVM model;
+            // Инициализираме модел
+            using (Db db = new Db())
+            {
+                SidebarDTO dto = db.Sidebars.Find(1);
+
+                model = new SidebarVM(dto);
+            }
+            //Връщаме модел с частично View
+            return PartialView("_SidebarPartial",model);
+        }
+
+    }
+}
